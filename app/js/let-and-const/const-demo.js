639347eeb016277389fbd324e7d6c56914947537
davidhe <davidhe@synnex.com>
2019-03-20 10:55:08 +0800

    const

--- app/js/let-and-const/const-demo.js
+++ app/js/let-and-const/const-demo.js
@@ -18,3 +18,16 @@ if (true) {
 }
 
 /*MAX */// Uncaught ReferenceError: MAX is not defined
+
+{
+	const foo = {};
+
+// 为 foo 添加一个属性，可以成功
+	foo.prop = 123;
+	foo.prop // 123
+
+// 将 foo 指向另一个对象，就会报错
+// 	foo = {}; // TypeError: "foo" is read-only
+
+	//常量foo储存的是一个地址，这个地址指向一个对象。不可变的只是这个地址，即不能把foo指向另一个地址，但对象本身是可变的，所以依然可以为其添加新属性。
+}
